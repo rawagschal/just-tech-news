d1ce9f88a7028db8446e5ec9cfe9c6478716a46e
Meat <47371524+rawagschal@users.noreply.github.com>
2020-11-08 18:42:12 -0800

    Comment model

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+// COMMENTS MODEL DATA FILE
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+// fields/columns for Comment model
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
