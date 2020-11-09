55057294a8625f9cbc2d408c7fa8b3d9ebdfddd3
Meat <47371524+rawagschal@users.noreply.github.com>
2020-11-08 20:05:39 -0800

    added comment-routes

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,54 @@
+// ROUTES TO WORK WITH COMMENT MODEL TO PERFOM CRUD OPS USING express.js
+
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const bodyParser = require('body-parser');
+
+// use body-parser middleware to parse req.body
+router.use(bodyParser.json());
+
+// get all comments 
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// create new comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+// delete comment by id
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
