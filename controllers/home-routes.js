b1509914887564d49783d6a5b6224b20987a6329
Meat <47371524+rawagschal@users.noreply.github.com>
2020-11-14 12:36:47 -0800

    added posts to home-routes and homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,44 @@
 // USER-FACING ROUTES
 
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    // use render() to specify template
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        console.log(dbPostData[0]);
+        //map each sequelize object into a serialized version to save in posts array
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        
+        res.render('homepage', { posts });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
