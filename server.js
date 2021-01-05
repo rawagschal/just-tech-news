2927f8f27787e8c5c7e7cda6d5adfc8a43a0f4d5
Rachel Wagschal <rawagschal@gmail.com>
2021-01-04 22:14:57 -0800

    create session, at 2.6

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ const routes = require('./controllers');
 
 // import connection to sequelize
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 // import handlebars
 const exphbs = require('express-handlebars');
