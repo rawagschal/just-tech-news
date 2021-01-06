fec61acd178fb69043c6d15b2d032712046f4de7
Rachel Wagschal <rawagschal@gmail.com>
2021-01-05 19:25:10 -0800

    logout btn

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -173,4 +173,19 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+//logout
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            //status code 204 = no content
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+
+});
+
 module.exports = router;
---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+// click event handler for /logout route
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        console.log('logged out');
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
